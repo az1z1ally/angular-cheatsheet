3e22d691e384be7378e90d33d5b26a1f97973e9f
Az1z1ally <albyally5@gmail.com>
2024-01-16 21:47:21 +0300

    Finished mat-card:

--- ng-mat/src/material.module.ts
+++ ng-mat/src/material.module.ts
@@ -8,14 +8,12 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatDividerModule} from '@angular/material/divider';  
 import { MatTabsModule } from '@angular/material/tabs'; 
 import { ClipboardModule } from '@angular/cdk/clipboard';
-
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
