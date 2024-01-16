864c26dfa18af3a8947ac5a0b756838928c59bc5
Az1z1ally <albyally5@gmail.com>
2024-01-16 20:40:34 +0300

    Created sidenav with breakpoint observer

--- ng-mat/src/app/components/menubar/menubar.component.ts
+++ ng-mat/src/app/components/menubar/menubar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-menubar',
@@ -7,8 +10,17 @@ import { Component } from '@angular/core';
 })
 export class MenubarComponent {
   badgeVisible: boolean = false
-  
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
   badgeVisibility(): void {
     this.badgeVisible = true
   }
+  
 }
